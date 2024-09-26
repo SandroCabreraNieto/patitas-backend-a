ec64acfb5852b9c831d232646c8fbe45a7ca79e5
Sandro Cabrera <cabrerasandro682@gmail.com>
2024-09-25 21:20:04 -0500

    Implementaciòn de simulaciòn de timeout

--- src/main/java/pe/edu/cibertec/patitas_backend/controller/AutenticacionController.java
+++ src/main/java/pe/edu/cibertec/patitas_backend/controller/AutenticacionController.java
@@ -7,6 +7,9 @@ import pe.edu.cibertec.patitas_backend.dto.LoginResponseDTO;
 import pe.edu.cibertec.patitas_backend.service.AutenticacionService;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.time.Duration;
+import java.util.Arrays;
 
 
 @RestController
@@ -20,13 +23,15 @@ public class AutenticacionController {
     public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequestDTO) {
 
         try {
+            Thread.sleep(Duration.ofSeconds(60));
             String[] datosUsuario = autenticacionService.validarUsuario(loginRequestDTO);
+            System.out.println("Resultado: " + Arrays.toString(datosUsuario));
             if (datosUsuario == null){
                 return new LoginResponseDTO("01", "Error: Usuario no encontrado","", "");
             }
             return new LoginResponseDTO("00", "",datosUsuario[0], datosUsuario[1]);
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             return new LoginResponseDTO("99", "Error: Ocurrio un problema","", "");
 
         }
