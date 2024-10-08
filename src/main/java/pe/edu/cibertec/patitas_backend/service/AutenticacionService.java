79c74c6c994dab4209c16bee5e0b0211f0466a87
Sandro Cabrera <cabrerasandro682@gmail.com>
2024-10-08 15:52:37 -0500

    Implementacion timeouts

--- src/main/java/pe/edu/cibertec/patitas_backend/service/AutenticacionService.java
+++ src/main/java/pe/edu/cibertec/patitas_backend/service/AutenticacionService.java
@@ -4,6 +4,7 @@ import pe.edu.cibertec.patitas_backend.dto.LoginRequestDTO;
 
 import java.io.IOException;
 
+
 public interface AutenticacionService {
 
     String[] validarUsuario(LoginRequestDTO loginRequestDTO) throws IOException;
